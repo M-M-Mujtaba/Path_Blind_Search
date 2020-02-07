748c7e5fa25e1555c2d8b2d78f6dd6257bb519c4
M-M-Mujtaba <l174144@lhr.nu.edu.pk>
2020-02-08 01:11:28 +0500

    State defined ,successor  defined .

--- Path_Finder.py
+++ Path_Finder.py
@@ -1,12 +1,24 @@
 import numpy as np
-from collections import namedtuple
+from dataclasses import dataclass
+import operator
 
-State = namedtuple('parent', 'Coord')
+
+@dataclass
+class State:
+    Parent: []
+    Current: []
+
+    def __eq__(self, other):
+        return self.Current == other.Current
 
 
 # Global variables
 dimensions = []
 grid = None
+right = [0, 1]
+up = [1, 0]
+di = [1, 1]
+
 
 def file_input(file_name):
     global dimensions
@@ -17,20 +29,46 @@ def file_input(file_name):
     file_lines = file_data.split("\n")
     dimensions = [int(i) for i in file_lines[0].split(" ")]
     start_cord = [int(i) for i in file_lines[1].split(" ")]
-    start_state = (start_cord, start_cord)
+
+    start_state = State(start_cord, start_cord)
+
     goal_cord = [int(i) for i in file_lines[2].split(" ")]
+    goal_state = State([], goal_cord)
     rows = dimensions[0]
     cols = dimensions[1]
     grid = np.zeros((rows, cols), int)
     for i in range(3, len(file_lines) - 2):
         grid[i - 3] = [int(j) for j in file_lines[i].split(" ")]
 
-    return start_state, goal_cord
-
-def successors(curr_state):
-    curr_coord = curr_state.Coord
+    return start_state, goal_state
 
 
+
+
+def successors(state):
+    States = []
+    global right
+    global up
+    global di
+    end = False
+    coord = state.Current
+
+    if coord[0] < dimensions[0] - 1 and coord[1] < dimensions[1] - 1:
+        right_state = State(state.Current, list(map(operator.add, right, state.Current)))
+        up_state = State(state.Current, list(map(operator.add, up, state.Current)))
+        di_state = State(state.Current, list(map(operator.add, di, state.Current)))
+
+        States.append(right_state)
+        States.append(up_state)
+        States.append(di_state)
+    elif coord[0] == dimensions[0] - 1 and coord[1] == dimensions[1] - 1:
+        end = True
+    elif coord[0] == dimensions[0] - 1:
+        States.append(State(state.Current, list(map(operator.add, right, state.Current))))
+    else:
+        States.append(State(state.Current, list(map(operator.add, up, state.Current))))
+
+    return States, end
 
 def main():
     global dimensions
