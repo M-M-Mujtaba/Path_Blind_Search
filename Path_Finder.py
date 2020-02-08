1e5f82748b4ea765f5c82fc83408d8b0e4e10747
M-M-Mujtaba <l174144@lhr.nu.edu.pk>
2020-02-08 17:48:07 +0500

    added comments

--- Path_Finder.py
+++ Path_Finder.py
@@ -1,6 +1,7 @@
 import numpy as np
 from dataclasses import dataclass
 import operator
+import watson_text_talker
 
 
 @dataclass
@@ -14,7 +15,7 @@ class State:
 
 # Global variables
 dimensions = []
-grid = None
+grid = None  # 2d array of all the information about the map
 right = [0, 1]
 up = [1, 0]
 di = [1, 1]
@@ -51,7 +52,7 @@ def successors(state):
     global right
     global up
     global di
-    end = False         # True when we reach the upper right corner (no where to go now )
+    # end = False  # True when we reach the upper right corner (no where to go now )
     coord = state.Current
 
     if coord[0] < dimensions[0] - 1 and coord[1] < dimensions[1] - 1:
@@ -62,14 +63,51 @@ def successors(state):
         States.append(right_state)
         States.append(up_state)
         States.append(di_state)
-    elif coord[0] == dimensions[0] - 1 and coord[1] == dimensions[1] - 1:
-        end = True
-    elif coord[0] == dimensions[0] - 1:
+    # elif coord[0] == dimensions[0] - 1 and coord[1] == dimensions[1] - 1:
+    # end = True
+    elif coord[0] == dimensions[0] - 2:
         States.append(State(state.Current, list(map(operator.add, right, state.Current))))
-    else:
+    elif coord[1] == dimensions[1] - 2:
         States.append(State(state.Current, list(map(operator.add, up, state.Current))))
 
-    return States, end
+    return States
+
+
+def print_path(path, start_state, goal_state):
+    global grid
+
+    for i in range(dimensions[0]):
+        for j in range(dimensions[1]):
+            if State([], [i, j]) == start_state:
+                print('S', end=" ")
+            elif State([], [i, j]) == goal_state:
+                print('G', end=" ")
+            elif State([], [i, j]) in path:
+                print('*', end=" ")
+            else:
+                print(grid[i][j], end=" ")
+        print(" ")
+
+    print("", end="\n \n \n")
+
+
+def bfs(start_state, goal_state):
+    global grid
+    found = False
+    path = []
+    visited = grid.copy()
+    path.append(start_state)
+    while len(path) > 0:
+        check_state = path.pop(0)
+        if check_state == goal_state:
+            found = True
+            print_path(path, start_state, goal_state)
+        visited[check_state.Current[0]][check_state.Current[1]] = 2
+        for state in successors(check_state):
+            if visited[state.Current[0]][state.Current[1]] < 1:
+                path.append(state)
+
+    return found
 
 
 def main():
@@ -77,6 +115,11 @@ def main():
     global grid
 
     start_state, goal_state = file_input('grid.txt')
+    print(start_state)
+    print(goal_state)
+    if not bfs(start_state, goal_state):
+        print("No path found")
+
     print(grid)
 
 
